e930ba40307d999426798e967926e088d27d7c4f
Michal Budzynski <michal@virtualdesign.pl>
2014-08-20 11:28:47 -0500

    [NPM-TOP-MD] - :hospital: first attempt to render the list

--- index.js
+++ index.js
@@ -5,31 +5,57 @@ var nas = require('npm-author-scrape');
 var gravatar = require('gravatar');
 var each = require('async').each;
 var fs = require('fs');
-var path = process.cwd();
+//var path = process.cwd();
 
 var authors = [];
 var ntm = function() {
   npmtop(function(err, all){
     authors = all.slice(0, 256);
     each(authors, function(user, cb) {
-      getGravatar(user.author, function(gravURL){
-        user.gravatar = gravURL;
+      getGravatar(user.author, function(npmAuthorData){
+        user.gravatar = npmAuthorData.gravURL;
+        user.fullName = npmAuthorData.name;
         cb();
       });
     }, generateList);
   });
 };
 
-var getGravatar = function(name, cb){
-  path;
-  fs;
+var getGravatar = function(name, cb) {
   nas(name, function(user) {
-    cb(gravatar.url(user.email, {s: '30', r: 'pg', d: '404'}));
+    cb({
+      name: user.full_name,
+      gravURL: gravatar.url(user.email, {s: '30', r: 'pg', d: '404'})
+    });
   });
 };
 
 var generateList = function() {
-  console.log(authors);
+  var content = authors.map(function(author) {
+    return generateOneRow(author);
+  }).join('\n');
+
+  var header = require('./static/header');
+  var footer = require('./static/footer');
+
+  fs.writeFile('./LIST.md', header + content + footer, function(err){
+    if (err) {
+      console.log('LYPA', err);
+    } else {
+      console.log('done!');
+    }
+  })
+};
+
+var generateOneRow = function(author){
+  return '<tr><th scope="row">' + author.rank +
+         '</th><td><a href="http://npmjs.org/~' + author.author +
+         '">' + author.author +
+         '</a> (' + author.fullName +
+         ')</td><td>' + author.packages +
+         '</td><td></td>' + ' Location ' +
+         '<td><img width="30" height="30" src="' + author. gravatr +
+         '"></td></tr>';
 };
 
 ntm();
