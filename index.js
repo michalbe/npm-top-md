fca78e70b401a5753e2b43f6dbcee3d1f27c13d5
Michal Budzynski <michal@virtualdesign.pl>
2014-08-20 10:36:09 -0500

    [NPM-TOP-MD] - :busts_in_silhouette: get all the users

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 'use strict';
 
-var ntm = function() {
+var npmtop = require('./npmtop');
 
+var ntm = function() {
+  npmtop(function(err, all){
+    all.forEach(function(us){
+      console.log(us.slice(0, 256));
+    });
+  });
 };
 
+var getAuthorData = function(name){
+  
+};
+
+ntm();
 module.exports = ntm;
